15b51771d89c0eee7ca3640ce5a11df9e0b56567
Ali Ebrahimi <aliebrahimi6739@gmail.com>
2022-02-14 01:15:10 +0330

    wait progress; select sound mode; send message

--- meeting.py
+++ meeting.py
@@ -1,19 +1,45 @@
+import time
+
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
 
 
-def session_running(href):
-    return "/join/" in str(href)
+class Meeting:
+    def __init__(self, browser: Chrome):
+        self.browser = browser
 
+    def join_meeting(self):
+        meetings = self.browser.find_elements(By.CSS_SELECTOR, ".newmeet a")
 
-def join_meeting(browser):
-    meetings = browser.find_elements(By.CSS_SELECTOR, ".newmeet a")
+        for meeting in meetings:
+            href = meeting.get_attribute("href")
 
-    for meeting in meetings:
-        href = meeting.get_attribute("href")
-        if session_running(href):
-            meeting.click()
-            # TODO select listen-only or mic modes
+            if "/join/" in str(href):
+                meeting.click()
+                return True
 
-            return f"joining current meeting.."
+        return False
 
-    return "no running meeting found.."
+    def wait_progress(self):
+        while True:
+            try:
+                self.browser.find_element(By.ID, "progressBar")
+                time.sleep(10)
+                self.browser.refresh()
+
+            except NoSuchElementException:
+                break
+
+    def listen_only(self):
+        self.browser.implicitly_wait(10)
+        self.browser.find_element(By.CLASS_NAME, "icon-bbb-listen").click()
+
+    def microphone(self):
+        self.browser.implicitly_wait(10)
+        self.browser.find_element(By.CLASS_NAME, "icon-bbb-unmute").click()
+
+    def send_message(self, msg):
+        self.browser.implicitly_wait(5)
+        self.browser.find_element(By.ID, "message-input").send_keys(msg)
+        self.browser.find_element(By.CLASS_NAME, "icon-bbb-send").click()
