51b704c23c144c6a0a02282c5f8c8cd51247017a
Ali Ebrahimi <aliebrahimi6739@gmail.com>
2022-02-15 17:41:45 +0330

    update driver options

--- driver.py
+++ driver.py
@@ -1,37 +1,46 @@
-from selenium import webdriver
+from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
-from config import USERNAME, PASSWORD
 
-
-class DriverOptions:
+class DriverOptions(Options):
     def __init__(self):
-        self.options = Options()
-        self.options.add_argument("start-maximized")
-        self.options.add_experimental_option("excludeSwitches", ["enable-automation"])
-        self.options.add_experimental_option('useAutomationExtension', False)
+        super().__init__()
+
+        self.add_argument("start-maximized")
+        self.add_argument("--ignore-certificate-errors")
+        self.add_argument("--ignore-ssl-errors")
+
+        self.add_argument('allow-file-access-from-files')
+        self.add_argument('use-fake-device-for-media-stream')
+        self.add_argument('use-fake-ui-for-media-stream')
+
+        self.add_experimental_option("detach", True)
+        self.add_experimental_option("excludeSwitches", ["enable-automation", "enable-logging"])
+        self.add_experimental_option("useAutomationExtension", False)
+        self.add_experimental_option("prefs", {"credentials_enable_service": False,
+                                               "profile.password_manager_enabled": False})
 
 
-class WebDriver(DriverOptions):
+class Browser(Chrome):
     def __init__(self):
-        DriverOptions.__init__(self)
-        self.browser = self.__get_browser()
+        super().__init__(options=DriverOptions(),
+                         service=Service(ChromeDriverManager(log_level=50).install()))
 
-    def __get_browser(self):
-        service = Service(ChromeDriverManager().install())
-        browser = webdriver.Chrome(service=service, options=self.options)
-        return browser
+
+class WebDriver:
+    def __init__(self):
+        self.browser = Browser()
 
     def load_browser(self):
         self.browser.get("http://lms.ui.ac.ir/")
 
-    def login_account(self):
+    def login_account(self, user):
         self.browser.find_element(By.ID, "annonmentc").click()
-        self.browser.find_element(By.ID, "username").send_keys(USERNAME)
-        self.browser.find_element(By.ID, "password").send_keys(PASSWORD)
+        self.browser.find_element(By.ID, "username").send_keys(user.username)
+        self.browser.find_element(By.ID, "password").send_keys(user.password)
         self.browser.find_element(By.ID, "submit").click()
         assert "errors" not in self.browser.page_source
 
